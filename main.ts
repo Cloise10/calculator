761f1c1dfb2df2324317eb5411b1ed86427a716c
Luna Nyx <79994302+Cloise10@users.noreply.github.com>
2021-03-05 13:06:17 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    num_1 += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    sum = 0 + 0
+})
+input.onButtonPressed(Button.B, function () {
+    num_2 += -1
+})
+input.onGesture(Gesture.Shake, function () {
+    diff = 0 + 0
+})
+let diff = 0
+let sum = 0
+let num_1 = 0
+let num_2 = 0
+sum = 0

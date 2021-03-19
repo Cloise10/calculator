21e28cf9ac4e83bc3736f157fcd503d2c6ceee3f
Luna Nyx <79994302+Cloise10@users.noreply.github.com>
2021-03-19 11:19:00 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,7 +12,33 @@ input.onButtonPressed(Button.A, function () {
     num_2 += 1
     basic.showNumber(num_2)
 })
-// on button A+B, stores 'num 1' then adds 'num 1' with 'num 2'
+// resets program to 0
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Target)
+    basic.showIcon(IconNames.Diamond)
+    basic.clearScreen()
+    num_1 = 0
+    num_2 = 0
+    sum = 0
+    diff = 0
+})
+// shows 'num 2' then subtracts 'num 1' with 'num 2'
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showString("num 2")
+    basic.pause(200)
+    basic.showNumber(num_2)
+    diff = num_1 - num_2
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showNumber(diff)
+})
+// on button A+B, stores 'num 1' 
 input.onButtonPressed(Button.AB, function () {
     basic.showString("num 1")
     basic.pause(200)
@@ -24,15 +50,6 @@ input.onButtonPressed(Button.AB, function () {
         . . # . .
         . . . . .
         `)
-    sum = num_1 + num_2
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . . . . .
-        . # # # .
-        . . . . .
-        `)
-    basic.showNumber(sum)
 })
 // on button A, 'num 1' and/or 'num 2' goes up by 1
 input.onButtonPressed(Button.B, function () {
@@ -60,7 +77,13 @@ input.onGesture(Gesture.Shake, function () {
         . . . . .
         . . . . .
         `)
-    diff = num_1 - num_2
+})
+// shows 'num 2' then adds 'num 1' with 'num 2'
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showString("num 2")
+    basic.pause(200)
+    basic.showNumber(num_2)
+    sum = num_1 + num_2
     basic.showLeds(`
         . . . . .
         . # # # .
@@ -68,17 +91,7 @@ input.onGesture(Gesture.Shake, function () {
         . # # # .
         . . . . .
         `)
-    basic.showNumber(diff)
-})
-// resets program to 0
-input.onGesture(Gesture.LogoDown, function () {
-    basic.showIcon(IconNames.SmallDiamond)
-    basic.showIcon(IconNames.Target)
-    basic.showIcon(IconNames.Diamond)
-    num_1 = 0
-    num_2 = 0
-    sum = 0
-    diff = 0
+    basic.showNumber(sum)
 })
 // shows name of program; sets all variables to 0
 let diff = 0
@@ -88,7 +101,6 @@ let num_1 = 0
 basic.showIcon(IconNames.Diamond)
 basic.showIcon(IconNames.Target)
 basic.showIcon(IconNames.SmallDiamond)
-basic.pause(200)
 basic.clearScreen()
 basic.showString("Calculator")
 num_1 = 0
